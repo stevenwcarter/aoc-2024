07edd473ee5f85c90cde3365669599a21cbc6041
Steven Carter <steve@javapl.us>
2024-12-08 19:34:35 -0500

    Fixing day 4 test

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -131,7 +131,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, Some(18));
+        assert_eq!(result, Some(17));
     }
 
     #[test]
