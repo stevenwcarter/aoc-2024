31a3147006bc3dda60419408323b963ba29d4168
Steven Carter <steve@javapl.us>
2024-12-10 17:23:33 -0500

    Day 10, adding test for part 2

--- src/bin/10.rs
+++ src/bin/10.rs
@@ -186,10 +186,17 @@ mod tests {
     }
 
     #[test]
-    fn test_part_two() {
+    fn test_part_two_2() {
         let result = part_two(&advent_of_code::template::read_file_part(
             "examples", DAY, 2,
         ));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(2));
+    }
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file_part(
+            "examples", DAY, 1,
+        ));
+        assert_eq!(result, Some(81));
     }
 }
